c668991445dab722afa510da65acba0c48c15dfb
Mikey Murphy <mikeyamadeo@gmail.com>
2015-10-12 17:32:16 -0600

    add ui

--- api-server.js
+++ api-server.js
@@ -1,74 +1,66 @@
-/**
- * Load dependencies
- */
-var express = require('express')
-var cors = require('cors')
-var db = require('./database.js')
-var app = express()
-app.use(cors()) // This lets us use different ports for API.
-var port = 4000
 
-/**
- * Send the HTML for the front end.
- */
-app.get('/', function (request, response) {
-  response.send('Welcome to the API server!')
-})
+var pokemon = [
+  {
+    id: 1,
+    trainerId: 1,
+    name: 'Pikachu',
+    level: 35
+  },
+  {
+    id: 2,
+    trainerId: 1,
+    name: 'Charizard',
+    level: 40
+  },
+  {
+    id: 3,
+    trainerId: 2,
+    name: 'Staryu',
+    level: 20
+  },
+  {
+    id: 4,
+    trainerId: 2,
+    name: 'Horsea',
+    level: 22
+  },
+  {
+    id: 5,
+    trainerId: 3,
+    name: 'Onyx',
+    level: 15
+  },
+  {
+    id: 6,
+    trainerId: 3,
+    name: 'Geodude',
+    level: 13
+  }
+];
 
-/* ***************************
- * Trainer-related API calls
- ****************************/
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var ROOT_DIR = "src/";
+var port = 4000;
 
-app.get('/api/trainers', function (request, response) {
-  response.json(db.getAllTrainers())
-})
+http.createServer(function (req, res) {
+  var urlObj = url.parse(req.url, true, false);
 
-/**
- * TODO: create an endpoint that gets a single trainer by their ID.
- * It needs to receive a GET request at '/api/trainers/:id'
- * and call the `db.getTrainerById` function.
- */
-// app.get('/api/trainers/:id', function (request, response) {
-//   response.json(db.getTrainerById(request.params.id))
-// })
+  if (urlObj.pathname === '/pokemon') {
+    res.writeHead(200)
+    res.end(JSON.stringify(pokemon))
+  } else {
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 
-/**
- * TODO: create an endpoint that adds a trainer to the database.
- * It needs to receive a POST request at '/api/trainers'
- * and call the `db.addTrainer` function.
- */
+}).listen(port);
 
-/* ***************************
- * Pokemon-related API calls
- ****************************/
-
-app.get('/api/pokemon', function (request, response) {
-  response.json(db.getAllPokemon())
-})
-
-app.get('/api/pokemon/:id', function (request, response) {
-  var pokemonId = request.params.id
-  response.json(db.getPokemonById(pokemonId))
-  /**
-   * TODO: finish implementing the `db.getPokemonById` function.
-   */
-})
-
-/**
- * TODO: create an endpoint that gets all the pokemon for a trainer.
- * It needs to receive a GET request at '/api/trainers/:id/pokemon'
- * and call the `db.getPokemonByTrainerId` function.
- */
-
- /**
-  * TODO: create an endpoint that adds a pokemon to the database.
-  * It needs to receive a POST request at '/api/pokemon'
-  * and call the `db.addPokemon` function.
-  */
-
-/**
- * Start the server.
- */
-var server = app.listen(port, function () {
-  console.log('API Server listening on port %s', port)
-})
